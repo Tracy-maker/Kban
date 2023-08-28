692cf1bc96dddae0085ced9c27a8ada0ddae6e3e
Tracy-maker <ydlvns@gmail.com>
2023-08-28 11:00:53 +1000

    fix buga

--- .history/components/Modal/Modal_20230828104609.tsx
+++ .history/components/Modal/Modal_20230828104609.tsx
@@ -10,8 +10,12 @@ function Modal() {
   ]);
 
   return (
+    // Use the `Transition` component at the root level
     <Transition appear show={isOpen} as={Fragment}>
       <Dialog as="form" className="relative z-10" onClose={closeModal}>
+        {/*
+          Use one Transition.Child to apply one transition to the backdrop...
+        */}
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
@@ -23,21 +27,26 @@ function Modal() {
         >
           <div className="fixed inset-0 bg-black bg-opacity-25" />
         </Transition.Child>
-        <div className="fixed inset-0 overflow-y-auto">
-          <div className="flex min-h-full items-center justify-center p-4 text-center">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              <div className="fixed inset-0 bg-black bg-opacity-25" />
-            </Transition.Child>
-          </div>
-        </div>
+
+        {/*
+          ...and another Transition.Child to apply a separate transition
+          to the contents.
+        */}
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          <Dialog.Panel>
+            <Dialog.Title>Deactivate account</Dialog.Title>
+
+            {/* ... */}
+          </Dialog.Panel>
+        </Transition.Child>
       </Dialog>
     </Transition>
   );
