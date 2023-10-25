741a17ec87bc0b226454cf357164fb27b90cede0
Tracy-maker <ydlvns@gmail.com>
2023-10-25 19:13:22 +1100

    1

---
+++ .history/components/Header/Header_20231025190754.tsx
@@ -0,0 +1,77 @@
+"use client";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/20/solid";
+import Avatar from "react-avatar";
+import { useBoardStore } from "@/store/BoardStore";
+import fetchSuggestion from "@/lib/fetchSuggesion";
+
+function Header() {
+  const [board, searchString, setSearchString] = useBoardStore((state) => [
+    state.board,
+    state.searchString,
+    state.setSearchString,
+  ]);
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [suggestion, setSuggestion] = useState<string>("");
+
+  useEffect(() => {
+    if (board.columns.size === 0) return;
+    setLoading(true);
+
+    const fetchSuggestionFunc = async () => {
+      const suggestion = await fetchSuggestion(board);
+      setSuggestion(suggestion);
+      setLoading(false);
+    };
+    fetchSuggestionFunc();
+  }, [board]);
+
+  return (
+    <header>
+      <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
+        <div className="absolute top-0 left-0 w-full h-4/5 bg-gradient-to-br from-pink-400 to-[#0055D1] rounded-md filter blur-3xl opacity-50 -z-50" />
+
+        <Image
+          className="w-46 md:w-62 pb-10 md:pb-0 object-contain"
+          src="https://i.ibb.co/72x6hgk/todolist-Logo.png"
+          alt="kanBan Logo"
+          width={300}
+          height={200}
+        />
+        <div className="flex items-center space-x-5 flex-1 justify-end w-full">
+          <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
+            <MagnifyingGlassIcon className="w-5 h-5 text-gray-500" />
+            <input
+              type="text"
+              placeholder="Searching..."
+              value={searchString}
+              onChange={(e) => setSearchString(e.target.value)}
+              className="flex-1 outline-none p-2"
+            />
+            <button type="submit" hidden>
+              Search
+            </button>
+          </form>
+          <Avatar name="Tracy Z" round size="50" color="#78b5e7" />
+        </div>
+      </div>
+      {/* chartGPT facing some trobles */}
+      {/* <div className="flex items-center justify-center px-5 py-2 md:py-5">
+        <p className="flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#78b5e7]">
+          <UserCircleIcon
+            className={`inline-block h-10 w-10 text-[#78b5e7] mr-1 ${
+              loading && "animate-spin"
+            }`}
+          />
+          {suggestion && !loading
+            ? suggestion
+            : "Now counting your tasks of the day..."}
+        </p>
+      </div> */}
+    </header>
+  );
+}
+
+export default Header;
